7ec2d5d55c772746443ebc8ec1229fe5ffa1e7cf
Bruna Coutinho <smarciabruna@gmail.com>
2025-02-10 15:50:30 -0300

    final

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de interatividade: Validação do formulário
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+  });
